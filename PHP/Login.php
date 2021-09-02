e7e9be11ca41fa054488755502b923f5dd8376d5
Alfred <54032594+spynetS@users.noreply.github.com>
2021-09-02 16:35:30 +0200

    Update Login.php

--- PHP/Login.php
+++ PHP/Login.php
@@ -16,17 +16,20 @@ $UsernameResult = $db->get("SELECT * FROM Users WHERE Username ='{$_POST['userna
 $password1 = "";
 $username = "";
 $email = "";
+//if user login in with email
 while($row = mysqli_fetch_array($EmailResult))
 {
    $password1=$row['Password'];
    $email = $row['Email'];
 
 }
+//if user loged in with username
 while($row = mysqli_fetch_array($UsernameResult))
 {
    $password1=$row['Password'];
    $username = $row['Username'];
 }
+//checks if email is correct with the password passed in
 if(($email==$UserNameEmail&&password_verify($password,$password1)))
 {
     $result = $db->get("SELECT * FROM Users WHERE Email ='{$_POST['username']}'");
@@ -34,6 +37,8 @@ if(($email==$UserNameEmail&&password_verify($password,$password1)))
     $_SESSION['username'] = mysqli_fetch_array($result)['Username'];
     $ut->setPage('/sharer/html/home.php');
 }
+//checks if username is correct with the password passed in
+
 else if($username==$UserNameEmail&&password_verify($password,$password1))
 {
     $_SESSION['username'] = $_POST['username'];
