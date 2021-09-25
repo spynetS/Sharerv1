8ed76acb8a4a14a1684440182ac90f30523ef747
Alfred <54032594+spynetS@users.noreply.github.com>
2021-09-25 12:36:35 +0200

    Added delete function in llibrary

--- PHP/DownloadImg.php
+++ PHP/DownloadImg.php
@@ -8,24 +8,37 @@ session_start();
 
 // Include the database configuration file  
 // Get image data from database 
-$id = $_POST['id'];
-$userid = getUserId($_SESSION['username']);
-$sql = "SELECT * FROM `{$userid}files` WHERE FileId ='{$id}'"; // 1
-$res = $dbb->sqli()->query($sql);
-while($row = $res->fetch_assoc())
-{ 
-    $type = pathinfo($row['TheFileName'], PATHINFO_EXTENSION);
-    $name = $row['TheFileName'];
-    $size =  $row['FileSize'];
-    $image = $row['FileData'];
-}
-header("Content-type: ".$type);
-header('Content-Disposition: attachment; filename="'.$name.'"');
-header("Content-Transfer-Encoding: binary"); 
-header('Expires: 0');
-header('Pragma: no-cache');
-header("Content-Length: ".$size);
+if(isset($_POST['Download']))
+{
 
-echo $image;
-exit();
+    $id = $_POST['id'];
+    $userid = getUserId($_SESSION['username']);
+    $sql = "SELECT * FROM `{$userid}files` WHERE FileId ='{$id}'"; // 1
+    $res = $dbb->sqli()->query($sql);
+    while($row = $res->fetch_assoc())
+    { 
+        $type = pathinfo($row['TheFileName'], PATHINFO_EXTENSION);
+        $name = $row['TheFileName'];
+        $size =  $row['FileSize'];
+        $image = $row['FileData'];
+    }
+    header("Content-type: ".$type);
+    header('Content-Disposition: attachment; filename="'.$name.'"');
+    header("Content-Transfer-Encoding: binary"); 
+    header('Expires: 0');
+    header('Pragma: no-cache');
+    header("Content-Length: ".$size);
+    
+    echo $image;
+    exit();    
+}
+else if(isset($_POST['Delete'])){
+    echo "delete";
+    //Delte row with file name
+    $db = new DataBase();
+    $userid= getUserId($_SESSION['username']); 
+    $id = $_POST['id'];
+    $result =$db->sqli()->query("DELETE FROM {$userid}files WHERE FileId='$id'");
+
+}
 
